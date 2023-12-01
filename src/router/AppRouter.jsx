8049f94319d8402ed624ccf19a0cc196858ff929
Jose <jflopezdesilva@gmail.com>
2023-12-01 22:11:16 +0100

    Proyecto calendar app front

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage } from "../auth/pages/LoginPage";
+import { CalendarPage } from "../calendar";
+
+export const AppRouter = () => {
+    const authState = "authenticated";
+
+    return (
+        <Routes>
+            {authState == "not-authenticated" ? (
+                <Route path="/auth/*" element={<LoginPage />} />
+            ) : (
+                <Route path="/*" element={<CalendarPage />} />
+            )}
+
+            <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </Routes>
+    );
+};
